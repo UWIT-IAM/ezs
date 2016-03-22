1405f9e5424df8e401dded2009b4c0f23cf5762c
jimfox <fox@washington.edu>
2016-03-22 12:54:46 -0700

    Add tests, set version to 1.4.2

--- test/runtests.sh
+++ test/runtests.sh
@@ -14,30 +14,55 @@ if [ $? -ne 0 ]; then ret=1; fi
 rm crypt1.txt crypt1.out
 
 
-# basic clinet/server
+# basic client/server
 echo "==== client/server test 1"
 rm -f session.pem
-rm -f server1.out
-rm -f client1.out
-rm -f client1.err
+rm -f server.out
+rm -f client.out
+rm -f client.err
 # start server
-./server1 -p 98765 -ca certs/ca.crt -c certs/server.crt -k certs/server.key -nt < /dev/null > server1.out 2>&1 &
+./server1 -p 98765 -ca certs/ca.crt -c certs/server.crt -k certs/server.key -nt < /dev/null > server.out 2>&1 &
 sleep 1
 # test bad CA fails
-echo "Hello, world. Fails by CA." | ./client1 -s localhost:98765 -ca certs/uwca.crt -c certs/client.crt -k  certs/client.key > client1.out 2> client1.err
+echo "Hello, world. Fails by CA." | ./client1 -s localhost:98765 -ca certs/uwca.crt -c certs/client.crt -k  certs/client.key > client.out 2> client.err
 # test new session
 echo "Hello, world. First time.
-Goodbye world." | ./client1 -s localhost:98765 -ca certs/ca.crt -c certs/client.crt -k  certs/client.key >> client1.out 2>> client1.err
+Goodbye world." | ./client1 -s localhost:98765 -ca certs/ca.crt -c certs/client.crt -k  certs/client.key >> client.out 2>> client.err
 # test recover session
 echo "Hello, world. Second time.
-exit" | ./client1 -s localhost:98765 -ca certs/ca.crt -c certs/client.crt -k  certs/client.key >> client1.out 2>> client1.err
+exit" | ./client1 -s localhost:98765 -ca certs/ca.crt -c certs/client.crt -k  certs/client.key >> client.out 2>> client.err
 
-diff -q  data/server1.out server1.out
+diff -q  data/server1.out server.out
 if [ $? -ne 0 ]; then ret=1; fi
-diff -q  data/client1.out client1.out
+diff -q  data/client1.out client.out
 if [ $? -ne 0 ]; then ret=1; fi
-diff -q  data/client1.err client1.err
+diff -q  data/client1.err client.err
 if [ $? -ne 0 ]; then ret=1; fi
 
+
+# non-blocking server test
+echo "==== client/server test 2"
 rm -f session.pem
+rm -f server.out
+rm -f client.out
+rm -f client.err
+# start server
+./server2 -p 98766 -ca certs/ca.crt -c certs/server.crt -k certs/server.key -nt < /dev/null > server.out 2>&1 &
+sleep 1
+# test new session
+echo "Hello, world. First time.
+Goodbye world." | ./client1 -s localhost:98766 -ca certs/ca.crt -c certs/client.crt -k  certs/client.key >> client.out 2>> client.err
+# test recover session
+echo "Hello, world. Second time.
+exit" | ./client1 -s localhost:98766 -ca certs/ca.crt -c certs/client.crt -k  certs/client.key >> client.out 2>> client.err
+
+diff -q  data/server2.out server.out
+if [ $? -ne 0 ]; then ret=1; fi
+diff -q  data/client2.out client.out
+if [ $? -ne 0 ]; then ret=1; fi
+diff -q  data/client2.err client.err
+if [ $? -ne 0 ]; then ret=1; fi
+rm -f session.pem
+
+
 exit $ret
